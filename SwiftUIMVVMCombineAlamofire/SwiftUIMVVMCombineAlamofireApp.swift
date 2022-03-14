1a10e6251329575d840b1c5a03a92f5a5c5f1ce0
RajendranEshwaran <rajendranmca2010@gmail.com>
2022-03-13 21:05:56 -0400

    Initial Commit

---
+++ SwiftUIMVVMCombineAlamofire/SwiftUIMVVMCombineAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMCombineAlamofireApp.swift
+//  SwiftUIMVVMCombineAlamofire
+//
+//  Created by gomathi saminathan on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMCombineAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
